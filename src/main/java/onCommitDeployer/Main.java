0490c38ba6917e9c3ac9dffbc41c3071bca17631
MaxTx01 <dujojawuy@tryzoe.com>
2018-10-24 16:52:20 +0300

    Moving server to spark

--- src/main/java/onCommitDeployer/Main.java
+++ src/main/java/onCommitDeployer/Main.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import org.apache.commons.io.IOUtils;
 import org.json.*;
+import spark.Spark;
 
 import java.io.InputStream;
 import java.net.InetSocketAddress;
@@ -11,24 +12,18 @@ import java.net.InetSocketAddress;
 public class Main {
     private static final int PORT = 7000;
     public static void main(String[] args) throws Throwable {
-        AppContainer container = new AppContainer();
-        container.rebuild();
+        //AppContainer container = new AppContainer();
+        //container.rebuild();
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
-        server.createContext("/", (HttpExchange t) ->
-        {
-            if(t.getRequestHeaders().getFirst("X-GitHub-Event").equals("push")) {
-                try (InputStream requestBodyStream = t.getRequestBody()) {
-                    String requestBody = IOUtils.toString(requestBodyStream, "utf-8");
-                    JSONObject reqJson = new JSONObject(requestBody);
-                    System.out.println("Rebuild after push from " + reqJson.getJSONObject("pusher").getString("name"));
-                    container.rebuild();
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
+        Spark.port(7000);
+        Spark.post("/api/events", (req, res) -> {
+            if(req.headers("X-GitHub-Event").equals("push")){
+                JSONObject reqJson = new JSONObject(req.body());
+                System.out.println("Rebuild after push from " + reqJson.getJSONObject("pusher").getString("name"));
+                //container.rebuild();
+                return "ok";
             }
+            return "";
         });
-        server.setExecutor(null);
-        server.start();
     }
 }
