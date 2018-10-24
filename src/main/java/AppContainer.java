158f90b9aabdb71a685d7e970508e628c6d4efcb
MaxTx01 <dujojawuy@tryzoe.com>
2018-10-24 14:59:18 +0300

    Add on commit rebuilder

---
+++ src/main/java/AppContainer.java
@@ -0,0 +1,34 @@
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerCertificateException;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.DockerException;
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.ContainerCreation;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class AppContainer {
+    private final DockerClient docker;
+    private String currentImageId;
+    private String currentContainerId;
+
+    public AppContainer() throws DockerCertificateException {
+        docker = DefaultDockerClient.fromEnv().build();
+    }
+
+    public void rebuild() throws DockerException, InterruptedException, IOException {
+        if(currentContainerId != null){
+            docker.killContainer(currentContainerId);
+            docker.removeContainer(currentContainerId);
+        }
+
+        if(currentImageId != null){
+            docker.removeImage(currentImageId);
+        }
+
+        currentImageId = docker.build(Paths.get("./docker"), "app-for-deploy");
+        ContainerCreation container = docker.createContainer(ContainerConfig.builder().image(currentImageId).build());
+        currentContainerId = container.id();
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import com.spotify.docker.client.DockerCertificateException;
+import com.spotify.docker.client.DockerException;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.io.IOUtils;
+import org.json.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    private static final int PORT = 7000;
+    public static void main(String[] args) throws Throwable {
+        AppContainer container = new AppContainer();
+        container.rebuild();
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        server.createContext("/", (HttpExchange t) ->
+        {
+            if(t.getRequestHeaders().getFirst("X-GitHub-Event").equals("push")) {
+                try (InputStream requestBodyStream = t.getRequestBody()) {
+                    String requestBody = IOUtils.toString(requestBodyStream, "utf-8");
+                    JSONObject reqJson = new JSONObject(requestBody);
+                    System.out.println("Rebuild after push from " + reqJson.getJSONObject("pusher").getString("name"));
+                    container.rebuild();
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        server.setExecutor(null);
+        server.start();
+    }
+}
