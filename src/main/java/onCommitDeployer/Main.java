b1f9406cfe4884a087c3b1cf82e86276141ceffa
MaxTx01 <dujojawuy@tryzoe.com>
2018-10-24 17:18:34 +0300

    Return dockerbuild

--- src/main/java/onCommitDeployer/Main.java
+++ src/main/java/onCommitDeployer/Main.java
@@ -12,15 +12,15 @@ import java.net.InetSocketAddress;
 public class Main {
     private static final int PORT = 7000;
     public static void main(String[] args) throws Throwable {
-        //AppContainer container = new AppContainer();
-        //container.rebuild();
+        AppContainer container = new AppContainer();
+        container.rebuild();
 
         Spark.port(7000);
         Spark.post("/api/events", (req, res) -> {
             if(req.headers("X-GitHub-Event").equals("push")){
                 JSONObject reqJson = new JSONObject(req.body());
                 System.out.println("Rebuild after push from " + reqJson.getJSONObject("pusher").getString("name"));
-                //container.rebuild();
+                container.rebuild();
                 return "ok";
             }
             return "";
